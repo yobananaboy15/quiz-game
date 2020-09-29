73df686495110b88f038a6dac7b877638131d6c3
Axel Thiel <axth2220@gmail.com>
2020-09-29 17:23:42 +0200

    the question class now works

--- js/Question.js
+++ js/Question.js
@@ -1,12 +1,32 @@
 class Question {
 
-    constructor(question){
-
+    constructor(questionObj){
+        this.questionElement = this.createQuestionElement(questionObj)
     }
 
-    createQuestionElement(){
-        //skapar en div
-        // tar data från den data som hämtastrycker in varje alternativ till vänster om en ett-checkbox-element
-        //det ska finnas lika många element som det finns frågor (längd på arrayen som hämtas.)
+    //skapar en div och fyller den med checkboxar och frågorna från det inskickade questionObj från konstruktorn.
+    createQuestionElement(questionObj){
+
+        const div = document.createElement('div')
+        div.append(questionObj.question)
+        div.append(document.createElement('br'))
+
+        Object.keys(questionObj.answers).forEach(key =>{
+
+            if(questionObj.answers[key]){
+                let input = document.createElement('input');
+                input.setAttribute('type', 'checkbox')
+                div.append(input)
+
+                //tar 7:e karaktären som är bokstaven för svarsalternativet
+                div.append(key[7] + ') ')
+            
+                div.append(questionObj.answers[key])
+            }
+        })
+
+
+        return div;
+
     }
 }
