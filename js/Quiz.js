cf6293fddebf06709654f3b563b4f0803d7b478c
Axel Thiel <axth2220@gmail.com>
2020-09-29 14:03:52 +0200

    added a quiz class and some potential methods

--- js/Quiz.js
+++ js/Quiz.js
@@ -1,3 +1,27 @@
 class Quiz{
-    
+    constructor(){
+        this.elementArray = [];
+        this.questionArray = []
+    }
+
+    correct(collection, answerArr){
+
+    }
+
+    fetchQuestions(){
+
+    }
+
+    createQuestionElement(){
+        //gör detta till en egen klass?
+        //skapar en div
+        //tar data från den data som hämtas
+        //trycker in varje alternativ till vänster om en ett-checkbox-element
+    }
+
+    updateInterface(){
+
+    }
+
+
 }
--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
-fetch("https://quizapi.io/api/v1/questions?limit=5", {
+fetch("https://quizapi.io/api/v1/questions?limit=20", {
   headers: {
     "X-Api-Key": "4LvQ5A2OeL1eEM1blNXiuf8L9UwUD8v4xhdceYnV",
   }
 })
 .then(response => response.json())
 .then(data => {
-    console.log(data)
+ 
 })
+
+//När användaren väljer hur många frågor som ska visas kör man en prompt som låt denne skriva in 5-10. Det värde används i query-strängen som skickas i requesten.
+
+//vilka klasser bör jag ha? var ska jag hålla reda på poäng?
+
+//kolla om obj.multiple_correct_answer === true. ovanligt 1 på 20.
+
+//det lättast är nog att skapa ett helt objekt med html som skickas in i diven. //hur ska svaren samlas? hur ska de kopplas till frågor?
+
+//ska knapparna för att submitta, gå fram/bak finnas från början?
 
 
 //API-token: 4LvQ5A2OeL1eEM1blNXiuf8L9UwUD8v4xhdceYnV
+
+//Lägg till eventlyssnare på knapp
+
+
+// 1. användaren skriver in hur många frågor (5-10)
+// 2. Värdet skickas som argument till någon metod tillhörande en klass som instansierats.
+// 3. Metoden hämtar frågorna genom fetch.
+// 4. Någon annan metod sätter ihop en lång html string med en checkbox för varje alternativ som inte är null.
+// 5. Användaren trycker på submit för att number 4 ska upprepas.
