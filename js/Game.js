ed37b200f071ee095f0ca308de76fb9ea37dd90c
Axel Thiel <axth2220@gmail.com>
2020-10-06 14:40:16 +0200

    removed the interface class and moved everything from there to game

--- js/Game.js
+++ js/Game.js
@@ -4,38 +4,114 @@ class Game {
         this.points = 0;
         this.rawQuestionData = questionData
         this.elementArray = new Questions(questionData);
-        //skapa interface-klass?
-        new Interface (this.elementArray) //skicka in questiondata?
+        //de här sakerna har egentligen inget med det här att göra.
+        this.currentQuestionIndex = 0;
+        this.newGame() //man skulle kunna köra den här i Interface-konstruktorn
 
     }
-    //behövs denna klass?
-    calculatePoints(){
-        //kan samarbeta med checkcorrect answers.s
+
+    setEventListeners(){
+        //sätter eventlisteners som kör sakerna nedan.
+        let nextButton = document.getElementById('next-btn')
+        let previousButton = document.getElementById('previous-btn')
+        let submitButton = document.getElementById('submit-btn')
+
+        nextButton.addEventListener('click', nextQuestion)
+        previousButton.addEventListener('click', previousQuestion)
+        submitButton.addEventListener('click', function(){
+            return kek;
+        });
+
+
+    }
+
+    newGame(){
+        this.setEventListeners();
+        document.getElementById('question-container').append(this.elementArray[0].questionElement)
+    }
+
+
+    
+    nextQuestion(){
+        
+        //const nextQuestion = () => nextQuestion()
+
+
+        if (!(this.currentQuestionIndex === this.elementArray.length - 1)){
+            document.getElementById('question-container').firstElementChild.replaceWith(this.elementArray[this.currentQuestionIndex + 1].questionElement)
+            this.currentQuestionIndex++
+        }
+        
+    }
+
+    previousQuestion(){
+
+        const previousQuestion = () => previousQuestion()
+
+        previousButton.addEventListener('click', previousQuestion)
+           //knapp för att gå bakåt en fråga index = current + 1
+        //kopplad till eventlistener på knapp
+        
+
+        
+        if(this.currentQuestionIndex){
+            document.getElementById('question-container').firstElementChild.replaceWith(this.elementArray[this.currentQuestionIndex - 1].questionElement)
+            this.currentQuestionIndex--
+        }
+        
+    }
+
+    endGame(elem){
+
+        
+
+        submitButton.addEventListener('click', function(){
+            let newArr = quiz.questionArray.map(element => element.correct_answers)
+            let result = game.checkCorrectAnswers(quiz.elementArray, newArr)
+            document.getElementById('question-container').append('You got ' + result + '/' + quiz.elementArray.length + ' questions right!')
+            //Dela upp i två funktioner
+            interface.endGame(startForm)
+            nextButton.removeEventListener('click', nextQuestion)
+            previousButton.removeEventListener('click', previousQuestion)
+            this.removeEventListener('click', arguments.callee)
+          });
+
+        
+
+        let btn = document.createElement('button');
+        btn.textContent = 'Click here to play again'
+        btn.addEventListener('click', e => {
+            document.getElementById('question-container').firstElementChild.replaceWith(elem);
+            document.getElementById('question-container').lastChild.remove(); //snygga till den här funktionen.
+
+        })
+        document.getElementById('question-container').firstElementChild.replaceWith(btn)
+        
     }
 
     checkCorrectAnswers(collection, answerArr){
-       //snygga till allt här.
-       
-       let playerAnswersCheckbox = collection.map(element => element.questionElement.querySelectorAll('input'))
-
-       let playerAnswerArray = playerAnswersCheckbox.map(checkboxCollection => Array.from(checkboxCollection, checkbox => checkbox.checked ? true : false))
-
-       let newAnswerArray = answerArr.map(element => {
-           return Object.values(element).map(value => value === 'true' ? true : false)
-       })
-
-       //loopa igenom varje array i playerAnswer array --> om samtliga värden i varje array i playerAnswer 
-       let testArr = playerAnswerArray.filter((element, index) => {
-           for(let i = 0; i < element.length; i++){
-               if (!element[i] === newAnswerArray[index][i])
-               return false
-           }
-           return true
-       });
-
-       return testArr.length
-       
-    }
+        //snygga till allt här.
+        
+        let playerAnswersCheckbox = collection.map(element => element.questionElement.querySelectorAll('input'))
+ 
+        let playerAnswerArray = playerAnswersCheckbox.map(checkboxCollection => Array.from(checkboxCollection, checkbox => checkbox.checked ? true : false))
+ 
+        let newAnswerArray = answerArr.map(element => {
+            return Object.values(element).map(value => value === 'true' ? true : false)
+        })
+ 
+        //loopa igenom varje array i playerAnswer array --> om samtliga värden i varje array i playerAnswer 
+        let testArr = playerAnswerArray.filter((element, index) => {
+            for(let i = 0; i < element.length; i++){
+                if (!element[i] === newAnswerArray[index][i])
+                return false
+            }
+            return true
+        });
+ 
+        return testArr.length
+        
+     }
 
 
 }
--- js/script.js
+++ js/script.js
@@ -9,55 +9,16 @@ document.addEventListener('DOMContentLoaded', e => {
     //ordna så att felmeddelande skrivs ut om man inte skrivit rätt.
     if (playerName && (NumberOfQuestions >= 5 && NumberOfQuestions <= 10)) {
       //lägg in try-catch
-      const questionData = await fetch(`https://quizapi.io/api/v1/questions?apiKey=4LvQ5A2OeL1eEM1blNXiuf8L9UwUD8v4xhdceYnV&limit=${numberOfQuestions}`)
-      const data = await response.json();
-      const game = new Game('axel', data);
+      const response = await fetch(`https://quizapi.io/api/v1/questions?apiKey=4LvQ5A2OeL1eEM1blNXiuf8L9UwUD8v4xhdceYnV&limit=${numberOfQuestions}`)
+      const questionData = await response.json();
+
+      const game = new Game('axel', questionData);
     }
-
-    // let playerName = document.getElementById('player-name')
-    // let NumberOfQuestions = document.getElementById('number-of-questions')
-    // let previousButton = document.getElementById('previous-btn')
-    // let nextButton = document.getElementById('next-btn')
-    // let submitButton = document.getElementById('submit-btn')
-
-    
-
-      // const quiz = new Questions();
-      //hära den här datan utanför objetet och initiera question med den.
-      // quiz.questionArray = await quiz.fetchQuestions(+NumberOfQuestions.value);
-      // quiz.createQuestionArray();
-
-      //använd för att kolla om det är rätt.
-      
-      
-      // const interface = new Interface(quiz.elementArray);
-
-      // const previousQuestion = () => interface.previousQuestion()
-      // const nextQuestion = () => interface.nextQuestion()
-      
-
-      //knapp för att gå bakåt en fråga index = current + 1
-      // previousButton.addEventListener('click', previousQuestion)
-
-    //Knapp för att gå framåt en fråga index = current - -1
-      // nextButton.addEventListener('click', nextQuestion)
+  
 
       // let startForm = e.target.parentNode
 
-      // submitButton.addEventListener('click', function(){
-      //   let newArr = quiz.questionArray.map(element => element.correct_answers)
-      //   let result = game.checkCorrectAnswers(quiz.elementArray, newArr)
-      //   document.getElementById('question-container').append('You got ' + result + '/' + quiz.elementArray.length + ' questions right!')
-
-
-        //game.checkCorrectAnswers() //ordna en array som innehåller objekt med rätt svar. kör filter - length för att få fram antal frågor som man svarat rätt på.
-        //kör funktion som kollar poäng
-      //   interface.endGame(startForm)
-      //   nextButton.removeEventListener('click', nextQuestion)
-      //   previousButton.removeEventListener('click', previousQuestion)
-      //   this.removeEventListener('click', arguments.callee)
-      // });
-
+    
       //tar bort formuläret och visar den första frågan.
       //spara form i variabel och använde igen för att starta om spelet. man kan köra append på 
 
@@ -68,9 +29,6 @@ document.addEventListener('DOMContentLoaded', e => {
       
   })
   
-    
 })
-
-//kolla om obj.multiple_correct_answer === true. ovanligt 1 på 20.
 
 
