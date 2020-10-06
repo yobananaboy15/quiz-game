2d4659307b6657934fafaa7251f26e337f3638f8
Axel Thiel <axth2220@gmail.com>
2020-10-06 11:14:12 +0200

    removed some methods from questions, fixed the chain between game-questions-question

--- js/Questions.js
+++ js/Questions.js
@@ -1,24 +1,14 @@
 class Questions{
-    constructor(){
-        //lägg player här?
-        this.elementArray = [];
-        this.questionArray = []; //hur får jag den att vänta här innan den går vidare. kör en asynkron metod i konstruktorn? kör i testbranch.
+    constructor(questionArray){
+        this.elementArray = this.createQuestionArray(questionArray);
     }
 
-
-    createQuestionArray(){ //skapa även correct questions array här?
-        //flytta for-loopen ovan hit. detta måste göras när datan är laddad.
-        for(let i = 0; i < this.questionArray.length; i++){
-            this.elementArray.push(new Question(this.questionArray[i]))
-        }   
-    }
-
-    async fetchQuestions(numberOfQuestions){
-
-        const response = await fetch(`https://quizapi.io/api/v1/questions?apiKey=4LvQ5A2OeL1eEM1blNXiuf8L9UwUD8v4xhdceYnV&limit=${numberOfQuestions}`)
-        const data = await response.json();
-        return data;
-
+    createQuestionArray(questionArray){ 
+        let elementArr = [];
+        for(let question of questionArray){
+            elementArr.push(new Question(question))
+        }
+        return elementArr   
     }
 
 }
--- js/script.js
+++ js/script.js
@@ -4,58 +4,67 @@ document.addEventListener('DOMContentLoaded', e => {
 
   document.getElementById('start-btn').addEventListener('click', async e =>{
 
-    let playerName = document.getElementById('player-name')
-    let NumberOfQuestions = document.getElementById('number-of-questions')
-    let previousButton = document.getElementById('previous-btn')
-    let nextButton = document.getElementById('next-btn')
-    let submitButton = document.getElementById('submit-btn')
-
+    let NumberOfQuestions = +document.getElementById('number-of-questions').value
+    let playerName = document.getElementById('player-name').value
     //ordna så att felmeddelande skrivs ut om man inte skrivit rätt.
-    if (playerName.value && (+NumberOfQuestions.value) >= 5 && +NumberOfQuestions.value <= 10) {
+    if (playerName && (NumberOfQuestions >= 5 && NumberOfQuestions <= 10)) {
+      //lägg in try-catch
+      const questionData = await fetch(`https://quizapi.io/api/v1/questions?apiKey=4LvQ5A2OeL1eEM1blNXiuf8L9UwUD8v4xhdceYnV&limit=${numberOfQuestions}`)
+      const data = await response.json();
+      const game = new Game('axel', data);
+    }
 
-      const quiz = new Questions();
+    // let playerName = document.getElementById('player-name')
+    // let NumberOfQuestions = document.getElementById('number-of-questions')
+    // let previousButton = document.getElementById('previous-btn')
+    // let nextButton = document.getElementById('next-btn')
+    // let submitButton = document.getElementById('submit-btn')
+
+    
+
+      // const quiz = new Questions();
       //hära den här datan utanför objetet och initiera question med den.
-      quiz.questionArray = await quiz.fetchQuestions(+NumberOfQuestions.value);
-      quiz.createQuestionArray();
+      // quiz.questionArray = await quiz.fetchQuestions(+NumberOfQuestions.value);
+      // quiz.createQuestionArray();
 
       //använd för att kolla om det är rätt.
-      const game = new Game('axel', 0);
       
-      const interface = new Interface(quiz.elementArray);
+      
+      // const interface = new Interface(quiz.elementArray);
 
-      const previousQuestion = () => interface.previousQuestion()
-      const nextQuestion = () => interface.nextQuestion()
+      // const previousQuestion = () => interface.previousQuestion()
+      // const nextQuestion = () => interface.nextQuestion()
       
 
       //knapp för att gå bakåt en fråga index = current + 1
-      previousButton.addEventListener('click', previousQuestion)
+      // previousButton.addEventListener('click', previousQuestion)
 
     //Knapp för att gå framåt en fråga index = current - -1
-      nextButton.addEventListener('click', nextQuestion)
+      // nextButton.addEventListener('click', nextQuestion)
 
-      let startForm = e.target.parentNode
+      // let startForm = e.target.parentNode
 
-      submitButton.addEventListener('click', function(){
-        let newArr = quiz.questionArray.map(element => element.correct_answers)
-        let result = game.checkCorrectAnswers(quiz.elementArray, newArr)
-        document.getElementById('question-container').append('You got ' + result + '/' + quiz.elementArray.length + ' questions right!')
+      // submitButton.addEventListener('click', function(){
+      //   let newArr = quiz.questionArray.map(element => element.correct_answers)
+      //   let result = game.checkCorrectAnswers(quiz.elementArray, newArr)
+      //   document.getElementById('question-container').append('You got ' + result + '/' + quiz.elementArray.length + ' questions right!')
 
 
         //game.checkCorrectAnswers() //ordna en array som innehåller objekt med rätt svar. kör filter - length för att få fram antal frågor som man svarat rätt på.
         //kör funktion som kollar poäng
-        interface.endGame(startForm)
-        nextButton.removeEventListener('click', nextQuestion)
-        previousButton.removeEventListener('click', previousQuestion)
-        this.removeEventListener('click', arguments.callee)
-      });
+      //   interface.endGame(startForm)
+      //   nextButton.removeEventListener('click', nextQuestion)
+      //   previousButton.removeEventListener('click', previousQuestion)
+      //   this.removeEventListener('click', arguments.callee)
+      // });
 
       //tar bort formuläret och visar den första frågan.
       //spara form i variabel och använde igen för att starta om spelet. man kan köra append på 
 
-      startForm.remove();
+      // startForm.remove();
 
-      interface.newGame()
-    }
+      // interface.newGame()
+   
       
   })
   
